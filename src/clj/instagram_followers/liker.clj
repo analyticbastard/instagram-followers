360087423bc8e3ec2fa6d56df358911c6f1890a0
analyticbastard <javierarrieropais@outlook.com>
2018-12-09 09:49:54 +0100

    Make liking asynchronous and more suitable dev config

--- src/clj/instagram_followers/liker.clj
+++ src/clj/instagram_followers/liker.clj
@@ -1,5 +1,5 @@
 (ns instagram-followers.liker
-  (:require [bonney.core :as bonney]
+  (:require [clojure.core.async :refer [go go-loop timeout <! >!! chan close!]]
             [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
             [instagram-followers.instagram :as instagram]))
@@ -13,19 +13,38 @@
        (take max-likes)
        (map instagram/id)))
 
+(defn- create-liker-service [instagram max-buff interval]
+  (let [ch (chan max-buff)]
+    (go-loop []
+      (when-let [post-id (<! ch)]
+        (instagram/like instagram post-id)
+        (<! (timeout interval))
+        (recur)))
+    ch))
+
+(defn- like-post! [ch post-id]
+  (>!! ch post-id))
+
+(defn- sleep [interval]
+  (try (Thread/sleep interval)
+       (catch InterruptedException _)))
+
 (defn make-like-handler [{:keys [max-users max-likes interval stats instagram] :as this}]
   (fn ! []
     (try (if-let [users (seq (instagram/get-users instagram))]
-           (let [num-users (rand-int max-users)]
+           (let [ch (create-liker-service instagram (inc (* max-users max-likes)) interval)
+                 num-users (rand-int max-users)]
              (swap! stats assoc :users (count users))
              (doseq [user (map #(% users) (repeat num-users rand-nth))
                      :let [profile (instagram/get-profile instagram user)
                            posts (get-posts-ids this profile)
-                           num-likes (min (count posts) (inc (rand-int max-likes)))]]
-               (doseq [post-id (map #(% posts) (repeat num-likes rand-nth))]
-                 (Thread/sleep interval)
-                 ;(instagram/like instagram post-id)
-                 (swap! stats update :likes + num-likes)))
+                           num-likes (min (count posts) (inc (rand-int max-likes)))
+                           post-ids (map #(% posts) (repeat num-likes rand-nth))]]
+               (doseq [post-id (set post-ids)]
+                 (like-post! ch post-id)
+                 (swap! stats update :likes + num-likes))
+               (sleep interval))
+             (close! ch)
              (log/info "Likes given in total" (-> stats deref :likes))
              true)
            (do
